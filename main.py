429479b085c86d8c4ffaaab0451b52bf9443ffa1
scythefly <fantasticfarm@hotmail.com>
2025-12-05 14:40:35 +0800

    .

--- main.py
+++ main.py
@@ -3,18 +3,8 @@
 import argparse
 import asyncio
 import time
-from template import build_ffmpeg_cmd
 from run import run_ffmpeg
-
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--template", required=True)
-    parser.add_argument("--input", required=True)
-    parser.add_argument("--output", required=True)
-    args = parser.parse_args()
-
-    cmd = build_ffmpeg_cmd(args.template, args.input, args.output)
-    print(cmd)
+from template import ffmpeg_cmd
 
 async def speed_aggregator(queue: asyncio.Queue, stop_event: asyncio.Event):
     speeds = []
@@ -42,7 +32,17 @@ async def speed_aggregator(queue: asyncio.Queue, stop_event: asyncio.Event):
 async def run():
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--concurrency", type=int, default=5, help="concurrency")
+    parser.add_argument("-t", "--template", type=str, default="uhd60", help="template name")
+    parser.add_argument("-l", "--list", action="store_true", help="list available templates")
     args = parser.parse_args()
+
+    if args.list:
+        from template import H264TEMPLATES, H265TEMPLATES
+        print("H264 Templates:")
+        print("/".join(H264TEMPLATES))
+        print("H265 Templates:")
+        print("/".join(H265TEMPLATES))
+        return
 
     tasks = []
     queue = asyncio.Queue()
@@ -52,7 +52,7 @@ async def run():
         t = asyncio.ensure_future(
             run_ffmpeg(
                 f"task{i+1}",
-                f'/home/test/5.4.0/ffmpeg-n7.1/ffmpeg -y  -hide_banner -hwaccel ni_quadra -force_nidec quadra -dec 15 -xcoder-params "out=hw:enableOut1=1:scale1=0x720" -y -vsync 0 -i /home/iuz/video/dota_60_1080.mp4 -filter_complex "ni_quadra_merge=filterblit=2" -force_key_frames source -c:v h265_ni_quadra_enc -xcoder-params "crfFloat=23.0:RcEnable=0:bitrate=3500000:vbvMaxRate=4000000:vbvBufferSize=1000:rdoLevel=1:lookAheadDepth=4:EnableRdoQuant=1:gopPresetIdx=5:enable2PassGop=0:minFramesDelay=1:enableVFR=1:repeatHeaders=1:GenHdrs=1:intraPeriod=0:zeroCopyMode=0:crfMaxIframeEnable=6:totalCuTreeDepth=6:cuTreeFactor=6" /home/iuz/output/out{i+1}.mp4',
+                ffmpeg_cmd(args.template, i),
                 queue
             )
         )
