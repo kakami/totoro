e66af985bb34de381c1b9c481538b3d1229ad437
scythefly <fantasticfarm@hotmail.com>
2025-12-09 17:13:43 +0800

    .

--- template.py
+++ template.py
@@ -33,32 +33,32 @@ def ffmpeg_cmd(template_name: str, input: str, index: int):
 
     # 264
     if template_name == "uhd60":
-        return f'{binary} {global_prefix} -i {input} -force_key_frames source  -c:v h264_ni_quadra_enc -xcoder-params "bitrate=5000000:vbvMaxRate=5200000:vbvBufferSize=1000:{param}" {output}'
+        return f'{binary} {global_prefix} -i {input} -force_key_frames source  -c:v h264_ni_quadra_enc -xcoder-params "bitrate=5000000:vbvMaxRate=5000000:vbvBufferSize=1000:{param}" {output}'
     elif template_name == "uhd":
-        return f'{binary} {global_prefix} -i {input} -filter_complex "fps=30" -force_key_frames source  -c:v h264_ni_quadra_enc -xcoder-params "bitrate=3500000:vbvMaxRate=3700000:vbvBufferSize=1000:{param}" {output}'
+        return f'{binary} {global_prefix} -i {input} -filter_complex "fps=30" -force_key_frames source  -c:v h264_ni_quadra_enc -xcoder-params "bitrate=3500000:vbvMaxRate=3500000:vbvBufferSize=1000:{param}" {output}'
     elif template_name == "hd60":
-        return f'{binary} {global_prefix} -i {input} -filter_complex "{_720p}" -force_key_frames source  -c:v h264_ni_quadra_enc -xcoder-params "bitrate=3000000:vbvMaxRate=3200000:vbvBufferSize=1000:{param}" {output}'
+        return f'{binary} {global_prefix} -i {input} -filter_complex "{_720p}" -force_key_frames source  -c:v h264_ni_quadra_enc -xcoder-params "bitrate=3000000:vbvMaxRate=3000000:vbvBufferSize=1000:{param}" {output}'
     elif template_name == "hd":
-        return f'{binary} {global_prefix} -i {input} -filter_complex "fps=30,{_720p}" -force_key_frames source  -c:v h264_ni_quadra_enc -xcoder-params "bitrate=1800000:vbvMaxRate=2000000:vbvBufferSize=1000:{param}" {output}'
+        return f'{binary} {global_prefix} -i {input} -filter_complex "fps=30,{_720p}" -force_key_frames source  -c:v h264_ni_quadra_enc -xcoder-params "bitrate=1800000:vbvMaxRate=1800000:vbvBufferSize=1000:{param}" {output}'
     elif template_name == "sd":
-        return f'{binary} {global_prefix} -i {input} -filter_complex "fps=25,{_540p}" -force_key_frames source -c:v h264_ni_quadra_enc -xcoder-params "bitrate=1200000:vbvMaxRate=1400000:vbvBufferSize=1000:{param}" {output}'
+        return f'{binary} {global_prefix} -i {input} -filter_complex "fps=25,{_540p}" -force_key_frames source -c:v h264_ni_quadra_enc -xcoder-params "bitrate=1200000:vbvMaxRate=1200000:vbvBufferSize=1000:{param}" {output}'
     elif template_name == "ld":
-        return f'{binary} {global_prefix} -i {input} -filter_complex "fps=20,{_360p}" -force_key_frames source -c:v h264_ni_quadra_enc -xcoder-params "bitrate=600000:vbvMaxRate=700000:vbvBufferSize=1000:{param}" {output}'
+        return f'{binary} {global_prefix} -i {input} -filter_complex "fps=20,{_360p}" -force_key_frames source -c:v h264_ni_quadra_enc -xcoder-params "bitrate=600000:vbvMaxRate=600000:vbvBufferSize=1000:{param}" {output}'
     elif template_name == "md":
-        return f'{binary} {global_prefix} -i {input} -filter_complex "fps=15,{_240p}" -force_key_frames source -c:v h264_ni_quadra_enc -xcoder-params "bitrate=300000:vbvMaxRate=400000:vbvBufferSize=1000:{param}" {output}'
+        return f'{binary} {global_prefix} -i {input} -filter_complex "fps=15,{_240p}" -force_key_frames source -c:v h264_ni_quadra_enc -xcoder-params "bitrate=300000:vbvMaxRate=300000:vbvBufferSize=1000:{param}" {output}'
     
     # 265
     elif template_name == "uhd560":
-        return f'{binary} {global_prefix} -i {input} -force_key_frames source  -c:v h265_ni_quadra_enc -xcoder-params "bitrate=5000000:vbvMaxRate=5200000:vbvBufferSize=1000:{param}" {output}'
+        return f'{binary} {global_prefix} -i {input} -force_key_frames source  -c:v h265_ni_quadra_enc -xcoder-params "bitrate=5000000:vbvMaxRate=5000000:vbvBufferSize=1000:{param}" {output}'
     elif template_name == "uhd5":
-        return f'{binary} {global_prefix} -i {input} -filter_complex "fps=30" -force_key_frames source  -c:v h265_ni_quadra_enc -xcoder-params "bitrate=3000000:vbvMaxRate=3200000:vbvBufferSize=1000:{param}" {output}'
+        return f'{binary} {global_prefix} -i {input} -filter_complex "fps=30" -force_key_frames source  -c:v h265_ni_quadra_enc -xcoder-params "bitrate=3000000:vbvMaxRate=3000000:vbvBufferSize=1000:{param}" {output}'
     elif template_name == "hd560":
-        return f'{binary} {global_prefix} -i {input} -filter_complex "{_720p}" -force_key_frames source  -c:v h265_ni_quadra_enc -xcoder-params "bitrate=2600000:vbvMaxRate=2800000:vbvBufferSize=1000:{param}" {output}'
+        return f'{binary} {global_prefix} -i {input} -filter_complex "{_720p}" -force_key_frames source  -c:v h265_ni_quadra_enc -xcoder-params "bitrate=2600000:vbvMaxRate=2600000:vbvBufferSize=1000:{param}" {output}'
     elif template_name == "hd5":
-        return f'{binary} {global_prefix} -i {input} -filter_complex "fps=30,{_720p}" -force_key_frames source  -c:v h265_ni_quadra_enc -xcoder-params "bitrate=1600000:vbvMaxRate=1700000:vbvBufferSize=1000:{param}" {output}'
+        return f'{binary} {global_prefix} -i {input} -filter_complex "fps=30,{_720p}" -force_key_frames source  -c:v h265_ni_quadra_enc -xcoder-params "bitrate=1600000:vbvMaxRate=1600000:vbvBufferSize=1000:{param}" {output}'
     elif template_name == "sd5":
-        return f'{binary} {global_prefix} -i {input} -filter_complex "fps=30,{_540p}" -force_key_frames source -c:v h265_ni_quadra_enc -xcoder-params "bitrate=1200000:vbvMaxRate=1400000:vbvBufferSize=1000:{param}" {output}'
+        return f'{binary} {global_prefix} -i {input} -filter_complex "fps=30,{_540p}" -force_key_frames source -c:v h265_ni_quadra_enc -xcoder-params "bitrate=1200000:vbvMaxRate=1200000:vbvBufferSize=1000:{param}" {output}'
     elif template_name == "ld5":
-        return f'{binary} {global_prefix} -i {input} -filter_complex "fps=20,{_360p}" -force_key_frames source -c:v h265_ni_quadra_enc -xcoder-params "bitrate=600000:vbvMaxRate=700000:vbvBufferSize=1000:{param}" {output}'
+        return f'{binary} {global_prefix} -i {input} -filter_complex "fps=20,{_360p}" -force_key_frames source -c:v h265_ni_quadra_enc -xcoder-params "bitrate=600000:vbvMaxRate=600000:vbvBufferSize=1000:{param}" {output}'
 
     raise ValueError(f"Unknown template: {template_name}")
